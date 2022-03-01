da21c664afb468b616a069931c4072b20fa04f64
vishnu gupta <vishnu.gupta@neopay.club>
2022-03-01 09:11:04 +0530

    fix placeholder

--- src/component/RegisterForm.js
+++ src/component/RegisterForm.js
@@ -39,7 +39,7 @@ const RegisterForm = ({ userType, handleBack }) => {
             < input className="inputbox" type={'text'} placeholder={`Enter ${userType} Name`} value={userName} onChange={e => setUserName(e.target.value)} />
             {
                 userType === 'Driver' &&
-                <input className="inputbox" type={'text'} placeholder={`Enter ${userType} id here..`} value={cabNumber} onChange={e => setCabNumber(e.target.value)} />
+                <input className="inputbox" type={'text'} placeholder={`Enter cab Number here..`} value={cabNumber} onChange={e => setCabNumber(e.target.value)} />
             }
             <input type={'button'} value={'Register'} className='btn' onClick={handleRegisterClick} />
             <input type={'button'} value={'Back'} className='btn' onClick={handleBack} />
